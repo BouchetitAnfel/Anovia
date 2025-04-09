1d06736ea97c44566b1174ae45d81a1174116c73
Hadil Benzaid <hadil.benzaid@univ-constantine2.dz>
2025-04-09 01:34:09 +0100

    Update CORS configuration and API routes

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:5173','http://127.0.0.1:8000'],
 
     'allowed_origins_patterns' => [],
 
